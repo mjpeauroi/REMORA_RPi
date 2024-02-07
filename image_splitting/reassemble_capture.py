1d540f877cc97b29e8344ec44110324bee5d7335
mjpeauroi <mjpeauroi@ucsb.edu>
2024-02-06 21:00:07 -0800

    added cleanup code for temporary files and automated finding num_buffers

--- image_splitting/reassemble_capture.py
+++ image_splitting/reassemble_capture.py
@@ -1,11 +1,21 @@
 import os
-import shutil
 
-num_buffers = 132
+# Adjust working directory
+working_directory = os.path.expanduser('~/Documents/REMORA_RPi/image_splitting')
+os.chdir(working_directory)
 
-# if os.path.exists("/split_capture"):
-#     shutil.rmtree("/split_capture")
-# os.makedirs("/split_capture")
+# Cleanup old reassembled_capture
+if os.path.exists("reassembled_capture.jpg"):
+    os.remove("reassembled_capture.jpg")
+    print("Deleted reassembled_capture.jpg")
+
+# Find the number of buffer files generated
+directory_path = os.path.expanduser('~/Documents/REMORA_RPi/image_splitting/split_capture/')
+if os.path.exists(directory_path):
+    # List everything in the directory
+    files = os.listdir(directory_path)
+    num_buffers = len(files)
+    print(f"{num_buffers} buffers found")
 
 # Reassemble the binary data from the split files
 reassembled_data = bytearray()
@@ -17,3 +27,4 @@ for i in range(num_buffers):
 # Save the reassembled data back to a JPEG file
 with open("reassembled_capture.jpg", 'wb') as output_file:
     output_file.write(reassembled_data)
+print("Created reassembled_capture.jpg")
--- image_splitting/split_capture_image.py
+++ image_splitting/split_capture_image.py
@@ -1,33 +1,45 @@
+# Program to capture a frame and split into buffer-sized binary files
+# Note [WARN:0] doesn't matter
+
 import cv2
 import os
 import shutil
 
+# Adjust working directory
+working_directory = os.path.expanduser('~/Documents/REMORA_RPi/image_splitting')
+os.chdir(working_directory)
 
 # Initialize the camera module and take a picture
 cam = cv2.VideoCapture(0)
 result, image = cam.read()
+print("Image captured")
 cam.release()  # Release the camera
 
 if result:
+
     # cleanup image and buffers from previous runs
     if os.path.exists("capture.jpg"):
         os.remove("capture.jpg")
         print("Deleted capture.jpg")
-    if os.path.exists("split_capture"):
-        shutil.rmtree("split_capture")
-        print("Deleted all files in split_capture")
-
-    cv2.imwrite("capture.jpg", image)
+    if os.path.exists("split_capture/"):
+        shutil.rmtree("split_capture/")
+        print("Deleted split_capture/")
     
     # Read the image as binary data
+    cv2.imwrite("capture.jpg", image)
+    print("Created capture.jpg")
     with open("capture.jpg", 'rb') as image_file:
         capture_bin = image_file.read()
     
     # Split into buffers
     buffer_size = 2048
     file_length = len(capture_bin)
+   
+    # Create new split_capture directory
+    os.makedirs("split_capture/", exist_ok=True)
+    print("Created split_capture/")
+
     buffer_number = 0
-    
     while (buffer_number * buffer_size < file_length):
         # Calculate the starting position for this buffer
         start_pos = buffer_number * buffer_size
@@ -42,3 +54,5 @@ if result:
         buffer_number += 1
 else:
     print("No image detected")
+
+print("Done.")
