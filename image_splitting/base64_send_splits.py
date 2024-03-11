6468924cfce30bf946a1523a14714fbfe4e64a2f
Ellie <mjpeauroi@ucsb.edu>
2024-03-11 01:18:26 +0000

    adjusted split code so everything is enumerated

--- image_splitting/base64_send_splits.py
+++ image_splitting/base64_send_splits.py
@@ -7,29 +7,42 @@ os.chdir(working_directory)
 
 
 # Find the number of buffer files generated
-directory_path = os.path.expanduser('~/Documents/REMORA_RPi/image_splitting/base64_split/')
+directory_path = os.path.expanduser('~/Documents/REMORA_RPi/image_splitting/base64_splits/')
 if os.path.exists(directory_path):
     # List everything in the directory
     files = os.listdir(directory_path)
     num_buffers = len(files)
     print(f"{num_buffers} buffers found")
 
+# Get the index label for this image
+directory_path = os.path.expanduser('~/Documents/REMORA_RPi/image_splitting/capture_archive/')
+if os.path.exists(directory_path):
+    # List everything in the directory
+    files = os.listdir(directory_path)
+    this_capture_index = len(files) - 1
 
-ser = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1)
+#ser = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1)
+ser = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1)
 
 try:
     # Open the serial port
     if not ser.is_open:
         ser.open()
-
+    buffer_to_send = f"<<START IMG {this_capture_index}>>\n"
+    ser.write(buffer_to_send.encode('ascii'))
+    print(f'Sent {buffer_to_send}')
     for i in range(num_buffers):
-        buffer_path = f"base64_split/split_{i}.txt"
+        buffer_path = f"base64_splits/split_{i}.txt"
         with open(buffer_path, 'r') as buffer_file:
             buffer_data = buffer_file.read()
-        buffer_to_send = "<<START>>" + buffer_data + "<<END>>" + '\n'
+        buffer_to_send = f"<<START-{i}>>" + buffer_data + f"<<END-{i}>>" + '\n'
         ser.write(buffer_to_send.encode('ascii'))
         print(f'Sent buffer {i} of {num_buffers}')
         time.sleep(0.1)
+    buffer_to_send = f"<<END IMG {this_capture_index}>>\n"
+    ser.write(buffer_to_send.encode('ascii'))
+    print(f'Sent {buffer_to_send}')
+
 
 finally:
     ser.close()
--- image_splitting/base64_split_capture.py
+++ image_splitting/base64_split_capture.py
@@ -6,7 +6,8 @@ import os
 import shutil
 import base64
 
-buffer_size = 512
+buffer_size = 1024
+compression_quality = 10
 
 def encode_to_base64(binary_data):
     base64_encoded_data = base64.b64encode(binary_data)
@@ -23,21 +24,35 @@ print("Image captured")
 cam.release()  # Release the camera
 
 if result:
-
     # cleanup image and buffers from previous runs
     if os.path.exists("capture.jpg"):
         os.remove("capture.jpg")
         print("Deleted capture.jpg")
-    if os.path.exists("split_capture/"):
-        shutil.rmtree("split_capture/")
-        print("Deleted split_capture/")
+    if os.path.exists("base64_splits/"):
+        shutil.rmtree("base64_splits/")
+        print("Deleted base64_splits/")
     
-    # Read the image as binary data
-    cv2.imwrite("capture.jpg", image)
-    print("Created capture.jpg")
-    with open("capture.jpg", 'rb') as image_file:
+    # # Read the image as binary data
+    # cv2.imwrite("capture.jpg", image)
+
+    # Get the index label for this image
+    directory_path = os.path.expanduser('~/Documents/REMORA_RPi/image_splitting/capture_archive/')
+    if os.path.exists(directory_path):
+        # List everything in the directory
+        files = os.listdir(directory_path)
+        this_capture_index = len(files)
+
+    height, width = image.shape[:2]
+    top_left_quarter = image[:height//2, :width//2]
+
+    # Compress and save the image
+    image_path = os.path.expanduser(f'~/Documents/REMORA_RPi/image_splitting/capture_archive/capture_{this_capture_index}.jpg')
+    cv2.imwrite(image_path, top_left_quarter, [int(cv2.IMWRITE_JPEG_QUALITY), compression_quality])
+
+    print(f"Created capture_{this_capture_index}.jpg")
+    with open(image_path, 'rb') as image_file:
         capture_bin = image_file.read()
-    
+
     # Convert using base64
     base64_data = (base64.b64encode(capture_bin)).decode("ascii")
 
@@ -46,8 +61,8 @@ if result:
     file_length = len(capture_bin)
    
     # Create new split_capture directory
-    os.makedirs("base64_split/", exist_ok=True)
-    print("Created base64_split/")
+    os.makedirs("base64_splits/", exist_ok=True)
+    print("Created base64_splits/")
 
     buffer_number = 0
     while (buffer_number * buffer_size < file_length):
@@ -55,11 +70,12 @@ if result:
         current_buffer = base64_data[start_pos:start_pos + buffer_size]
         
         # Write the buffer to its own text file
-        path = f"base64_split/split_{buffer_number}.txt"
+        path = f"base64_splits/split_{buffer_number}.txt"
         with open(path, 'w') as buffer_file:
             buffer_file.write(current_buffer)
         
         buffer_number += 1
+    print(f"Saved {buffer_number} buffer txt files.")
 
 else:
     print("No image detected")
